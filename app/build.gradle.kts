60565d3a5952607ec0341c981cb1c7e957960189
kennyb1201 <kmbwob1201617@gmail.com>
2026-07-31 00:33:44 -0400

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
 
-    implementation("androidx.tv:tv-material:1.0.0")
+    implementation("androidx.tv:tv-material:1.0.0") {
+            exclude(group = "androidx.tv", module = "tv-foundation")
+    }
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
