78871f7cf2608f855412994a41504bb950e3578a
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-01 09:29:15 -0400

    Remove unused import for lazy item

--- app/src/main/java/com/kennyb1201/kbstream/ui/tag/TagScreen.kt
+++ app/src/main/java/com/kennyb1201/kbstream/ui/tag/TagScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.item
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
