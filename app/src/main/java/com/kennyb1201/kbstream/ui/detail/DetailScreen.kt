5be5d6ad546a170945cf55812e3b2c0c8dd8a940
kennyb1201 <kmbwob1201617@gmail.com>
2026-07-31 22:53:31 -0400

    Add onNavigateTag parameter to DetailScreen

--- app/src/main/java/com/kennyb1201/kbstream/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/kennyb1201/kbstream/ui/detail/DetailScreen.kt
@@ -98,6 +98,7 @@ fun DetailScreen(
     onNavigateDetail: (String, String) -> Unit = { _, _ -> },
     onNavigateActor: (Int) -> Unit = {},
     onNavigateStudio: (Int, String, Boolean) -> Unit = { _, _, _ -> },
+    onNavigateTag: (Int, String, Boolean, String) -> Unit = { _, _, _, _ -> },
     onNavigateStreams: (StreamsTarget, String, String, String?) -> Unit = { _, _, _, _ -> },
     viewModel: DetailViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
