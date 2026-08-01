4eb65aea23c7186144bc32dba6e0d22403847c38
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-01 18:52:09 -0400

    Implement SimklApiService interface for API calls

---
+++ app/src/main/java/com/kennyb1201/kbstream/data/simkl/SimklApiService.kt
@@ -0,0 +1,37 @@
+package com.kennyb1201.kbstream.data.simkl
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface SimklApiService {
+
+    @GET("oauth/pin")
+    suspend fun createPinCode(
+        @Query("redirect") redirect: String = "urn:ietf:wg:oauth:2.0:oob"
+    ): SimklPinCodeResponse
+
+    @GET("oauth/pin/{userCode}")
+    suspend fun checkPin(
+        @Path("userCode") userCode: String
+    ): SimklTokenResponse
+
+    @GET("sync/activities")
+    suspend fun getActivities(
+        @Header("Authorization") authorization: String
+    ): SimklActivitiesResponse
+
+    @GET("sync/playback")
+    suspend fun getPlayback(
+        @Header("Authorization") authorization: String
+    ): List<SimklPlaybackItem>
+
+    @GET("sync/all-items/shows/watching")
+    suspend fun getWatchingShowsRaw(
+        @Header("Authorization") authorization: String,
+        @Query("date_from") dateFrom: String? = null,
+        @Query("extended") extended: String? = "full"
+    ): ResponseBody
+}
