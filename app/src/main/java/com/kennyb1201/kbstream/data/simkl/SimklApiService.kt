75259469ef5afa69e4e1ee1ad7acb32139a7cc57
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-01 17:47:04 -0400

    Create SimklApiService.kt

---
+++ app/src/main/java/com/kennyb1201/kbstream/data/simkl/SimklApiService.kt
@@ -0,0 +1,29 @@
+package com.kennyb1201.kbstream.data.simkl
+
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface SimklApiService {
+
+    @GET("oauth/pin")
+    suspend fun createPinCode(
+        @Query("redirect") redirect: String = "urn:ietf:wg:oauth:2.0:oob"
+    ): SimklPinCodeResponse
+
+    @GET("oauth/pin/{userCode}")
+    suspend fun checkPin(
+        @Path("userCode") userCode: String
+    ): SimklTokenResponse
+
+    @GET("sync/activities")
+    suspend fun getActivities(
+        @Header("Authorization") authorization: String
+    ): SimklActivitiesResponse
+
+    @GET("sync/playback")
+    suspend fun getPlayback(
+        @Header("Authorization") authorization: String
+    ): List<SimklPlaybackItem>
+}
