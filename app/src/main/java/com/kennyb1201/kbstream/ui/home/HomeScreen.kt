dfebf0808eec94907e0cf36a2ffd7e64d6321154
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-05 14:17:19 -0400

    Add remember import to HomeScreen.kt

--- app/src/main/java/com/kennyb1201/kbstream/ui/home/HomeScreen.kt
+++ app/src/main/java/com/kennyb1201/kbstream/ui/home/HomeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.remember
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
