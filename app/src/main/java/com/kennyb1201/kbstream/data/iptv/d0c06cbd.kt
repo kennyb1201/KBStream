66543ac5be18164997f49429117c150a18627cc2
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-16 16:55:07 -0400

    Add files via upload

---
+++ app/src/main/java/com/kennyb1201/kbstream/data/iptv/d0c06cbd.kt
@@ -0,0 +1,32 @@
+package com.kennyb1201.kbstream.data.iptv.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [EpgChannelEntity::class, EpgProgramEntity::class],
+    version = 2,
+    exportSchema = false
+)
+abstract class IptvDatabase : RoomDatabase() {
+    abstract fun iptvDao(): IptvDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: IptvDatabase? = null
+
+        fun getInstance(context: Context): IptvDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    IptvDatabase::class.java,
+                    "iptv_epg.db"
+                ).fallbackToDestructiveMigration().build().also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }
+}
