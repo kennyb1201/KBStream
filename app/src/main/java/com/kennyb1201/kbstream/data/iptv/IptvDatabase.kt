43aa21aa50d3211078c6f05a0c58d5b2a43174db
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-17 08:16:55 -0400

    Increment database version from 5 to 6

--- app/src/main/java/com/kennyb1201/kbstream/data/iptv/IptvDatabase.kt
+++ app/src/main/java/com/kennyb1201/kbstream/data/iptv/IptvDatabase.kt
@@ -11,7 +11,7 @@ import androidx.room.RoomDatabase
         EpgProgramEntity::class,
         CachedPlaylistChannelEntity::class
     ],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 abstract class IptvDatabase : RoomDatabase() {
