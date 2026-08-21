1b2ee592e6f60f01d9c8c639b3f98ad5539bfb37
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-21 16:35:25 -0400

    Refactor AddonManager constructor to be public

--- app/src/main/java/com/kennyb1201/kbstream/data/addon/AddonManager.kt
+++ app/src/main/java/com/kennyb1201/kbstream/data/addon/AddonManager.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
-class AddonManager private constructor(
+class AddonManager(
     context: Context
 ) {
 
