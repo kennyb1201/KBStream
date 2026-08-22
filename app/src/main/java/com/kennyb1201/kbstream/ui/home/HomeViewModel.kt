9a4049030e9ff55f0869584fb8f268b39a9fbff7
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-22 13:03:26 -0400

    Add collectLatest import to HomeViewModel

--- app/src/main/java/com/kennyb1201/kbstream/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/kennyb1201/kbstream/ui/home/HomeViewModel.kt
@@ -26,6 +26,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.Semaphore
