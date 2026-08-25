83ef1cd7ee2587b1a7c28619c1b06146db8c9b75
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-25 10:39:18 -0400

    Add NewPipeExtractor dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+implementation("com.github.TeamNewPipe:NewPipeExtractor:v0.24.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
