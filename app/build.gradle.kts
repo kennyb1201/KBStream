bbdf55cb481ac9b9d8f5978a607a142b4407f912
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-25 12:42:40 -0400

    Fix syntax for buildConfigField in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,9 @@ android {
         versionCode = 1
         versionName = "0.1.0"
 
-        buildConfigField("String", "TMDB_API_KEY", ""$tmdbApiKey"")
-        buildConfigField("String", "SIMKL_CLIENT_ID", ""$simklClientId"")
-        buildConfigField("String", "SIMKL_CLIENT_SECRET", ""$simklClientSecret"")
+        buildConfigField("String", "TMDB_API_KEY", ""${tmdbApiKey}"")
+buildConfigField("String", "SIMKL_CLIENT_ID", ""${simklClientId}"")
+buildConfigField("String", "SIMKL_CLIENT_SECRET", ""${simklClientSecret}"")
     }
 
     buildFeatures {
