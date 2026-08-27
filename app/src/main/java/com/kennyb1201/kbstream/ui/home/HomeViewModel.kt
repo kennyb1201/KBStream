e56b8973ab69862bc7616a62ef75ee9d31843510
kennyb1201 <kmbwob1201617@gmail.com>
2026-08-27 14:39:04 -0400

    Fix runtime assignment in HomeViewModel

--- app/src/main/java/com/kennyb1201/kbstream/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/kennyb1201/kbstream/ui/home/HomeViewModel.kt
@@ -1726,7 +1726,7 @@ private suspend fun calculateEpisodesRemaining(
 
  runtimeMinutes =
     matchedResumeEpisode
-        .runtime
+        .runtime,
 
                     episodesRemaining =
     calculateEpisodesRemaining(
